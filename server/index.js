30d5f69fed4afa5cd6044d8639b62e2a5a02ad18
EricLam404 <lameric9888@gmail.com>
2024-06-16 19:15:54 +0800

    chore: Update server index.js to use Apollo Server for GraphQL API

--- server/index.js
+++ server/index.js
@@ -2,43 +2,14 @@ const express = require("express");
 require("dotenv").config();
 const port = process.env.PORT || 3001;
 
-const schema = require("./schema/schema");
-const { GraphQLID, graphql } = require("graphql");
-const { createHandler } = require("graphql-http/lib/use/express");
-const { ruruHTML } = require("ruru/server");
-
-const app = express();
-
-// The root provides a resolver function for each API endpoint
-// let root = {
-//     hello: ({ name }) => {
-//         return "Hello, " + name;
-//     },
-
-//     user: () => {
-//         return {
-//             id: 1,
-//             name: "Sam",
-//         };
-//     },
-// };
-
-app.get("/", (_req, res) => {
-    res.type("html");
-    res.end(ruruHTML({ endpoint: "/graphql" }));
-});
-
-app.all(
-    "/graphql",
-    createHandler({
-        schema: schema,
-    })
-);
-
-// Start the server at port
-app.listen(
-    port,
-    console.log(
-        `Running a GraphQL API server at http://localhost:${port}/graphql`
-    )
-);
+import { ApolloServer } from "@apollo/server"
+import { startStandaloneServer } from "@apollo/server/standalone"
+ 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+ 
+const { url } = await startStandaloneServer(server)
+ 
+console.log(`🚀 Server ready at ${url}`)
