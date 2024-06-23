65f97eb0f1c33b7d826c4c8845f5dc6cf0a258f8
EricLam404 <lameric9888@gmail.com>
2024-06-23 10:51:26 +0800

    feat: Add Toaster component to RootLayout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Header from "./_components/Header";
+import { Toaster } from "@components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
             >
                 <Header />
                 {children}
+                <Toaster />
             </body>
         </html>
     );
