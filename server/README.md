741ddfdadea1339e8712ad24211dc080cf214d09
EricLam404 <lameric9888@gmail.com>
2024-09-18 21:48:58 -0400

    refactor: Update server package dependencies and improve authentication error handling
    - Updated package.json dependencies to include the latest versions of concurrently and supports-color
    - Improved authentication error handling in Apollo Server

--- server/README.md
+++ server/README.md
@@ -2,3 +2,7 @@
 1. ```npx prisma db push```
 2. ```npx supabase db reset```
 3. ```npx supabase gen types --lang=typescript --local > src/database.types.ts```
+
+
+# Update Schema
+1. ```npm run generate```
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,47 @@
-import { ApolloServer } from "@apollo/server";
-import { expressMiddleware } from "@apollo/server/express4";
-import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import express from "express";
 import dotenv from "dotenv";
 import http from "http";
 import cors from "cors";
-import { typeDefs } from "./schema/typeDefs.generated.ts";
-import { resolvers } from "./schema/resolvers.generated.ts";
-import supabaseClient from "./utils/db/supabase.ts";
-import parseCookies from "./utils/parseCookie.ts";
-import { MyContext } from "./utils/types/context.ts";
+import './utils/config/logging';
+
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { typeDefs } from "./schema/typeDefs.generated";
+import { resolvers } from "./schema/resolvers.generated";
+import { getServiceSupabase } from "./utils/db/supabase";
+import prisma from "./utils/db/prisma";
+import parseCookies from "./utils/middleware/parseCookie";
+import { MyContext } from "./utils/types/context";
 import { GraphQLError } from "graphql";
+import { SERVER_HOSTNAME, SERVER_PORT } from "./utils/config/config";
+import { loggingHandler } from "./utils/middleware/loggingHandler";
 
 dotenv.config();
-const port = process.env.PORT || 4000;
 
 const app = express();
-app.use(parseCookies());
-
 const httpServer = http.createServer(app);
+
+logging.log("----------------------------------------");
+logging.log("Initializing API");
+logging.log("----------------------------------------");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+logging.log("----------------------------------------");
+logging.log("Logging & Configuration");
+logging.log("----------------------------------------");
+app.use(parseCookies());
+app.use(loggingHandler);
+
+logging.log("----------------------------------------");
+logging.log("Starting Server");
+logging.log("----------------------------------------");
 
 const server = new ApolloServer<MyContext>({
     typeDefs,
     resolvers,
+    introspection: true,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
@@ -38,27 +57,42 @@ app.use(
     expressMiddleware(server, {
         context: async ({ req }): Promise<MyContext> => {
             if (!req.cookies["sb-127-auth-token"]) {
-                throw new GraphQLError(
-                    "Please Authenticate yourself.",
-                    {
-                        extensions: {
-                            code: "UNAUTHENTICATED",
-                        },
-                    }
-                );
+                throw new GraphQLError("User is not authenticated", {
+                    extensions: {
+                        code: "UNAUTHENTICATED",
+                        http: { status: 401 },
+                    },
+                });
             }
             const token = req.cookies["sb-127-auth-token"].access_token;
-            const supabase = supabaseClient(token);
+            const supabase = getServiceSupabase();
+            const {
+                data: { user },
+            } = await supabase.auth.getUser(token);
+
+            if (!user) {
+                throw new GraphQLError("User is not authenticated", {
+                    extensions: {
+                        code: "UNAUTHENTICATED",
+                        http: { status: 401 },
+                    },
+                });
+            }
             return {
                 cookies: req.cookies,
                 supabase,
+                prisma,
+                user,
             };
         },
     })
 );
 
 await new Promise<void>((resolve) =>
-    httpServer.listen({ port: port }, resolve)
+    httpServer.listen({ port: SERVER_PORT }, resolve)
 );
 
-console.log(`🚀 Server ready at http://localhost:${port}/graphql`);
+// console.log(`Server started on ${SERVER_HOSTNAME}:${SERVER_PORT}/graphql`);
+logging.log("----------------------------------------");
+logging.log(`Server started on ${SERVER_HOSTNAME}:${SERVER_PORT}/graphql`);
+logging.log("----------------------------------------");
