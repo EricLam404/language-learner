d7d0ed851c651ddebd778681e73fabacc77a8bea
EricLam404 <lameric9888@gmail.com>
2024-10-02 13:59:44 -0400

    Refactor createStory mutation resolver to handle multiple tags and include tags in the response

--- server/src/schema/story/resolvers/Mutation/createStory.ts
+++ server/src/schema/story/resolvers/Mutation/createStory.ts
@@ -1,8 +1,22 @@
 import { GraphQLError } from "graphql";
 import type { MutationResolvers } from "./../../../types.generated";
-export const createStory: NonNullable<MutationResolvers['createStory']> = async (_parent, _arg, _ctx) => {
+export const createStory: NonNullable<
+    MutationResolvers["createStory"]
+> = async (_parent, _arg, _ctx) => {
     /* Implement Mutation.createStory resolver logic here */
     try {
+        let createTags;
+        if (_arg.input.tags.length > 1) {
+            createTags = _arg.input.tags?.map((tag) => ({
+                create: { name: tag },
+                where: { name: tag },
+            }));
+        } else {
+            createTags = {
+                create: { name: _arg.input.tags[0] },
+                where: { name: _arg.input.tags[0] },
+            };
+        }
         const result = await _ctx.dataSources.prisma.story.create({
             data: {
                 userId: _ctx.user.id,
@@ -10,14 +24,16 @@ export const createStory: NonNullable<MutationResolvers['createStory']> = async 
                 content: _arg.input.content,
                 languageName: _arg.input.languageName,
                 description: _arg.input.description,
-                tags: {
-                    connectOrCreate: _arg.input.tags?.map((tag) => ({
-                        where: { name: tag },
-                        create: { name: tag },
-                    })),
-                },
                 difficulty: _arg.input.difficulty,
                 readCount: 0,
+                ...(_arg.input.tags.length > 0 && {
+                    tags: {
+                        connectOrCreate: createTags,
+                    },
+                }),
+            },
+            include: {
+                tags: true,
             },
         });
         return result;
--- server/src/schema/story/resolvers/Story.ts
+++ server/src/schema/story/resolvers/Story.ts
@@ -1,4 +1,15 @@
-import type   { StoryResolvers } from './../../types.generated';
-    export const Story: StoryResolvers = {
+import type { StoryResolvers } from "./../../types.generated";
+export const Story: StoryResolvers = {
     /* Implement Story resolver logic here */
-  };
+    tags: async (_parent, _arg, _ctx, _info) => {
+        return await _ctx.dataSources.prisma.tag.findMany({
+            where: {
+                stories: {
+                    some: {
+                        id: Number(_parent.id),
+                    },
+                },
+            },
+        });
+    },
+};
