2a6865ace908004b4358619362a0a91c6f7525f4
EricLam404 <lameric9888@gmail.com>
2024-10-21 21:58:02 -0400

    Refactor codegen to allow for client query/mutation introspection to generate types

--- server/src/utils/config/config.ts
+++ server/src/utils/config/config.ts
@@ -10,6 +10,7 @@ export const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_P
 export const supabaseUrl = process.env.SUPABASE_URL as string;
 export const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string;
 export const supabaseServiceRoleKey = process.env.SUPBASE_SERVICE_ROLE_KEY as string;
+export const isProduction = process.env.NODE_ENV === 'production';
 
 export const supabase = {
     supabaseUrl,
